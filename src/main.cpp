e986e37b56668479387bb9a8f31a55d3146b750e
protarie125 <proarie@gmail.com>
2022-02-25 23:54:29 +0900

    초기 커밋.

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	auto x = int{ -1 };
+	auto y = int{ -1 };
+	auto m = int{ 0 };
+	for (int i = 0; i < 9; ++i) {
+		for (int j = 0; j < 9; ++j) {
+			int v;
+			cin >> v;
+
+			if (m < v) {
+				x = j;
+				y = i;
+				m = v;
+			}
+		}
+	}
+
+	cout << m << '\n'
+		<< y + 1 << ' ' << x + 1;
+
+	return 0;
+}
